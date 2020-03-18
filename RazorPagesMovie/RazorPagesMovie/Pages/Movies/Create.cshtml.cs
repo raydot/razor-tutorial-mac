f99f830c6d2107f8668d680af67db65ee8d17550
Kanter <dave.kanter@BKTSFMAC03.local>
2020-03-18 09:52:23 -0700

    Working app and DB.

---
+++ RazorPagesMovie/RazorPagesMovie/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesMovieContext _context;
+
+        public CreateModel(RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet() // Initializes any state needed for the page.
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync() // Run when the page posts form data
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
